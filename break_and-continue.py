3906309634154782c0d557a84aa3502a6a130716
tangelas00 <tsampson77@gmail.com>
2023-08-10 04:43:01 +0000

    break and continue

---
+++ break_and-continue.py
@@ -0,0 +1,14 @@
+
+for item in range (1, 6) :
+    if item == 3:
+        break
+    print (item )
+print ("stop")
+
+
+    
+while True:
+    number = float(input( "Enter a number : "))
+    if number < 0:
+        break
+    print("You Entered: ", number )
